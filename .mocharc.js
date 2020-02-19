c36422eaa040dcd769f2d4764a0e3fec8e8a7eb3
Marcus Spiegel <spiegel@uscreen.de>
2020-02-19 13:20:14 +0100

    migrated mocha.opts -> mocharc.js

---
+++ .mocharc.js
@@ -0,0 +1,15 @@
+// migrated from mocha.opts with:
+// 
+// --require test/mocha.bootstrap.js
+// --reporter spec
+// --ui bdd
+// --recursive
+// --exit
+
+module.exports = {
+  require: ['test/mocha.bootstrap.js'],
+  reporter: 'spec',
+  ui: 'bdd',
+  recursive: true,
+  exit: true
+};
